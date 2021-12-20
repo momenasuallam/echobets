1b8a001aff582bdb0b1e3d78592b40459faff2d2
momenasuallam <59420500+momenasuallam@users.noreply.github.com>
2021-12-20 16:22:21 +0500

    add images and files

---
+++ js/app.js
@@ -0,0 +1,18 @@
+window.addEventListener("scroll", () => {
+  let header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+$(document).ready(function () {
+  // Toggle menu on click
+  $("#menu-toggler").click(function () {
+    toggleBodyClass("menu-active");
+  });
+
+  function toggleBodyClass(className) {
+    document.body.classList.toggle(className);
+  }
+});
+
+
+        // wow Animation
+        new WOW().init();
